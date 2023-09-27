628a576e0d7477dda6b85a62d854ae17bf8e150c
Iamneo <techadmin@iamneo.ai>
2023-09-27 10:53:00 +0000

    Backup Commit

---
+++ my-spring-project/src/main/java/com/example/MyAppConfig.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.example")
+public class MyAppConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("examly");
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public EmpDao empDao() {
+        EmpDao empDao = new EmpDao();
+        empDao.setTemplate(jdbcTemplate());
+        return empDao;
+    }
+  
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}
